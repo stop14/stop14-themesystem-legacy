dfef232db4ddda97ef6f07fa0e0b91674f8d4fa6
Bill Kennedy <bill@agilehumanities.ca>
2023-05-02 17:00:01 -0400

    Bug fix

--- scripts/scaffold.sh
+++ scripts/scaffold.sh
@@ -12,11 +12,11 @@ then
   if [ $# -eq 2 ] && [ $2 == "rebuild" ]
     then
       yes | cp -rf ./node_modules/stop14-themesystem-legacy/scaffolding/$1/* ./
-      yes | cp -rf ./node_modules/stop14-themesystem-legacy/scaffolding/$1/.* ./
+      yes | cp -rf ./node_modules/stop14-themesystem-legacy/scaffolding/$1/.eslintrc ./
 
     else
       cp -rn ./node_modules/stop14-themesystem-legacy/scaffolding/$1/* ./
-      cp -rn ./node_modules/stop14-themesystem-legacy/scaffolding/$1/.* ./
+      cp -rn ./node_modules/stop14-themesystem-legacy/scaffolding/$1/.eslintrc ./
 
     fi
 fi
