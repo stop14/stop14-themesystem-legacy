fdfb569979cf86ac0012db857fd5b15087611752
Bill Kennedy <bill@agilehumanities.ca>
2024-01-09 16:23:42 -0500

    Expose imageRatio.js DOM containers to local scripts

--- source/js/imageRatio.js
+++ source/js/imageRatio.js
@@ -6,49 +6,53 @@
  *  Requires: imagesloaded and ev-emitter packages (load via NPM).
  *
  */
-(function($) {    
+(function($) {
 
   $(document).ready(function() {
-    
+
     // An instance has been found where the imagesLoaded function is returning undefined.
     // This is not consistent – so far it only appears when the Universal Viewer is loaded.
     // @todo: Continue to investigate. In the meantime perform the required checks.
-    
-    $('main, #splash, #title').each(function() {	    
-      if (typeof $(this).imagesLoaded === 'function') {  
-        $(this).imagesLoaded(function(){    
+
+    // Allow local scripts to define containers for image aspect ratio application.
+
+    const scope = typeof imageARScope !== "undefined" ? imageARScope : 'main, #splash, #title';
+
+    $(scope).each(function() {
+      if (typeof $(this).imagesLoaded === 'function') {
+        $(this).imagesLoaded(function(){
           $(this.images).each(function(i,o) {
             var img = $(o.img);
-                  
+
             var h = o.img.naturalHeight;
             var w = o.img.naturalWidth
             var aspectClass = getImageAspectClass(h,w);
-                
+
             img.addClass('img-' + aspectClass);
-            
+
             img.closest('figure').addClass('figure-' + aspectClass);
-            
+
             if (img.parent('a').length > 0) {
               img.parent('a').addClass('a-' + aspectClass);
             }
-          });        
+          });
         });
       } else {
-	    	console.log('imagesLoaded JS library not found.')
+        console.log('imagesLoaded JS library not found.')
       }
     });
-  
+
   });
-  
+
   function getImageAspectClass(h,w) {
     var aspectClass = 'square';
-    
+
     if (w > h) {
       aspectClass = 'landscape';
     } else if (h > w) {
       aspectClass = 'portrait';
     }
-    
+
     return aspectClass;
   }
 })(jQuery);
